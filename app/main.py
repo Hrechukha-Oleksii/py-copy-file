f6eaf18a3ccc698528f89e8d6fc81f2c97569da6
Oleksii-Hrechukha <Oleksii_Hrechukha@outlook.com>
2025-08-30 13:45:45 +0300

    NewSolution2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-import os.path
-
-
 def copy_file(command: str) -> None:
 
     command_list = command.split(" ")
@@ -13,9 +10,9 @@ def copy_file(command: str) -> None:
     destination_file_name = command_list[2]
     if source_file_name == destination_file_name:
         return
-    if not os.path.exists(source_file_name):
-        return
-
-    with (open(source_file_name, "r") as file_in,
-          open(destination_file_name, "w") as file_out):
-        file_out.write(file_in.read())
+    try:
+        with (open(source_file_name) as file_in,
+              open(destination_file_name, "w") as file_out):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        pass
